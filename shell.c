4462c12177d5e5aa2ea28acefcf8d15858505020
utkarsht <utkarsh.trivedi11@gmail.com>
2015-09-19 07:56:31 +0530

    commited

--- shell.c
+++ shell.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 void main(int argc,char ** argv)
 {
